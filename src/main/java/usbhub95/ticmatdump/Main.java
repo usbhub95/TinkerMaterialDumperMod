05a2d5c251695e86dc3261e9a994adbbfeb17d9a
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2024-08-05 16:44:07 +1000

    add meta class

---
+++ src/main/java/usbhub95/ticmatdump/Main.java
@@ -0,0 +1,9 @@
+package usbhub95.ticmatdump;
+
+import net.minecraftforge.fml.common.Mod;
+import usbhub95.ticmatdump.util.Meta;
+
+@Mod(modid = Meta.MOD_ID, useMetadata = Meta.USE_METADATA)
+public class Main {
+    
+}
---
+++ src/main/java/usbhub95/ticmatdump/util/Meta.java
@@ -0,0 +1,6 @@
+package usbhub95.ticmatdump.util;
+
+public class Meta {
+    public static final String MOD_ID = "ticmatdump";
+    public static final boolean USE_METADATA = true;
+}
