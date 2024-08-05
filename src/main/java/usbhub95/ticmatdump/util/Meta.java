610ed9b2de633cef237f7067f8741388158aea5a
Cooper Lockrey <43819486+usbhub95@users.noreply.github.com>
2024-08-05 16:51:47 +1000

    add proxies to metadata class

--- src/main/java/usbhub95/ticmatdump/util/Meta.java
+++ src/main/java/usbhub95/ticmatdump/util/Meta.java
@@ -1,6 +1,13 @@
 package usbhub95.ticmatdump.util;
 
 public class Meta {
+    // metadata class for info given to @Mod
+    private Meta() {
+        throw new IllegalStateException("Attempted to instantiate the Meta class.");
+    }
+
     public static final String MOD_ID = "ticmatdump";
     public static final boolean USE_METADATA = true;
+    public static final String COMMON_PROXY = "usbhub95.ticmatdump.proxy.CommonProxy";
+    public static final String CLIENT_PROXY = "usbhub95.ticmatdump.proxy.ClientProxy";
 }
